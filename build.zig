883e4da8edb16b07952020d7b345bcb74b81189e
eugene <eugene_personal@pm.me>
2024-07-15 23:03:31 -0400

    b.createModule --> b.addModule

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     //
-    const kernelmaster_zig = b.createModule(.{
+    const kernelmaster_zig = b.addModule("kernelmaster", .{
         .root_source_file = b.path("./src/kernelmaster.zig"),
     });
     //
